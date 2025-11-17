1ba8521fec27a41e95978dae0658af2dfd21a43b
boewii_93 <lalitha12m143@gmail.com>
2025-11-17 21:33:47 +0530

    added files

---
+++ css/js/script.js
@@ -0,0 +1,17 @@
+alert("Hello World");
+console.log("Code is running....")
+console.log("Code is also running....")
+console.log("Code is looking like a wow....")
+// Variables
+var a=prompt("Enter your number:")
+var isTrue=confirm("Are you sure you want to leave this page and burn ur page and blast ur computer")
+if(isTrue){
+    console.log("Computer blasted.")
+}
+else{
+    console.log("Computer isn't blasting.")
+}
+console.log("Your number is " + a)
+
+document.title = "Hey I am good"
+document.body.style.background="red"
