dd663ec7b77090971444926e5d577d7f372632f2
mwomi-765 <lalitha12m143@gmail.com>
2025-11-23 21:14:16 +0530

    Added file

--- js/loops.js
+++ js/loops.js
@@ -4,3 +4,34 @@ let a=1;
 for(let i=0;i<10;i++){
     console.log(a+i);
 }
+
+let obj={
+    name: "hello",
+    role: "Programmer",
+    comapny: "Code"
+}
+
+// for in loop
+for(const key in obj){
+    console.log(key)
+}
+// for of loop
+for(const c of "hello"){
+    console.log(c)
+}
+
+//while-loop
+i=0;
+while(i<6){
+    console.log(i)
+    i++;
+}
+
+
+// do-while loop
+let i=10;
+do {
+    console.log(i)
+    i++;
+}
+while (i<6);
