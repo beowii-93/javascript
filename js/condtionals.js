a02f697a34f4691d10ce0f4bf80f3a415a9ac486
mwomi-765 <lalitha12m143@gmail.com>
2025-11-23 16:11:02 +0530

    added files

--- js/condtionals.js
+++ js/condtionals.js
@@ -1,1 +1,30 @@
-console.log("hello")
+console.log("hello this is a conditional statements")
+let age=5;
+let grace =2;
+if(age>18){
+    console.log("You can drive.");
+}
+else if(age==0){
+    alert("Enter valid age.");
+}
+else{
+    console.log("You can't drive.")
+}
+
+//Operators
+let area=9;
+console.log(area+grace);
+console.log(area-grace);
+console.log(area/grace);
+console.log(area**grace);
+console.log(area%grace);
+
+//special cases
+console.log("3"==3) //checks the value
+console.log("3"===3) //checks the value and reference and the type.
+
+// Ternary operator
+let a=9,b=3;
+let c=a>b ? a : b;
+console.log(c);
+
---
+++ js/loops.js
@@ -0,0 +1,6 @@
+console.log("Loops tutorial.")
+let a=1;
+
+for(let i=0;i<10;i++){
+    console.log(a+i);
+}
