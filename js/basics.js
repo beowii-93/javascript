393c224e9852bda995d67cfba46176c0b0c932e7
mwomi-765 <lalitha12m143@gmail.com>
2025-12-08 18:01:32 +0530

    Added files

--- js/basics.js
+++ js/basics.js
@@ -1,9 +1,11 @@
+// prompt(), alert(), and confirm() are browser-specific functions.
 alert("Hello World");
 console.log("Code is running....")
 console.log("Code is also running....")
 console.log("Code is looking like a wow....")
 // Variables
 var a=prompt("Enter your number:")
+
 var isTrue=confirm("Are you sure you want to leave this page and burn ur page and blast ur computer")
 if(isTrue){
     console.log("Computer blasted.")
@@ -11,7 +13,7 @@ if(isTrue){
 else{
     console.log("Computer isn't blasting.")
 }
-console.log("Your number is " + a)
+console.log("Your number is " + b)
 
 document.title = "Hey I am good"
 document.body.style.background="red"
--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,23 @@
-console.log("Javascript is a programming language.")
+function program(name) {
+    console.log("Hey " + name + " is a programming language.")
+}
+console.log("Hey Javascript is a programming language.")
+program("C")
+program("Python")
+
+function sum(a, b) {
+    // console.log(a+b)
+    return a + b;
+}
+
+result = sum(3, 5)
+console.log("The sum is:"+result)
+
+//Arrow function
+const add = (a, b) => a + b;
+// or
+const func1 =(x)=>{
+    console.log("This is arrow function",x)
+}
+func1(34);
+
